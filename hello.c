b9b4e8485110bb238b47d5f05a556fcac3661755
jimwilmes32 <46799908+jimwilmes32@users.noreply.github.com>
2019-01-17 18:49:35 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -12,7 +12,13 @@ int main(int argc, char **argv) {
 
   printf("Mike Wilmes\n");
   
-  printf("I am a Mechanical Engineering Major");
+  printf("I am a Mechanical Engineering Major\n");
+  
+  printf("Jim Wilmes\n");
+ 
+  
+  printf("I am a Mechanical Engineering Major\n");
+  
   
 
   return 0;
