aeb2a6084ab209aa4822a5bae5a60df84ce2f29d
mhwilmes <46800643+mhwilmes@users.noreply.github.com>
2019-01-17 18:36:02 -0600

    Added Major
    To add a line to change name

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Mike and Jim Wilmes 
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+
+  return 0;
+}
